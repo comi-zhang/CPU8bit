6853d3ce4e0a7ce007bc460f8382cda8626e670e
Comi <comi2805532913@gmail.com>
2024-05-31 00:10:34 +0800

    1400warings

--- t2.vhd
+++ t2.vhd
@@ -3,16 +3,18 @@ use ieee.std_logic_1164.all;
 
 entity t2 is
     port(
-        offset_8    : in std_logic_vector(7 downto 0);      -- 8位偏移量输入
-        offset_16   : out std_logic_vector(7 downto 0)      -- 8位偏移量输出
+        offset_4    : in std_logic_vector(3 downto 0);      -- 8位偏移量输入
+        offset_8   : out std_logic_vector(7 downto 0)      -- 8位偏移量输出
     );
 end t2;
 
 architecture behave of t2 is
 begin
-    process(offset_8)
+    process(offset_4)
     begin
         -- 将输入的偏移量直接输出
-        offset_16 <= offset_8;
+        if offset_4(3) = '1' then offset_8 <= "1111" &offset_4;
+		else offset_8 <= "0000" & offset_4;
+		end if;
     end process;
 end behave;
